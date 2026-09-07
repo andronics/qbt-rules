e816af15a4f3032437a365834be0179b62dcc8cf
Stephen Cox <andronics@gmail.com>
2026-09-07 14:44:42 +0100

    chore: Bump version to 0.5.5

--- src/qbt_rules/__version__.py
+++ src/qbt_rules/__version__.py
@@ -1,5 +1,5 @@
 """Version information for qbt-rules"""
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 __author__ = "qbt-rules"
 __description__ = "A powerful Python-based rules engine for qBittorrent automation with client-server architecture"
